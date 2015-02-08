a72f61102541ea49c60d30299fde409547d93fa8
unknown <deboltj@umich.edu>
2015-02-08 15:58:35 -0500

    loginz and shizz

--- gitmoneygitpaid/urls.py
+++ gitmoneygitpaid/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
 from login import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.windex, name='windex'),
-	(r'^login/$', 'django.contrib.auth.views.login'),
+	url(r'^login/$', 'django.contrib.auth.views.login'),
+	url(r'^accounts/profile/$', views.accountHome, name='account')
 )
--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 def index(request):
 	return HttpResponse("Hello World!")
@@ -8,3 +9,24 @@ def windex(request):
 	thing="fUcker"
 	context = {'thing': thing}
 	return render(request, 'index.html', context)
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return redirect(r'^home/')
+        else:
+            return HttpResponse("fuck")
+    else:
+        return HttpResponse("Invalid login, someone call the cops")
+
+def accountHome(request):
+    if request.user.is_authenticated():
+        user = request.user.username
+        context = {'user' : user}
+        return render(request, 'profile.html', context)
+    else:
+        return redirect(r'^login/')
