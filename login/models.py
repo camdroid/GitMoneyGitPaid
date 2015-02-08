c47db172d22783b0c90c1624678c086b236bb1e3
Cam <camherringshaw@gmail.com>
2015-02-08 15:06:45 -0500

    Added user to db

--- login/models.py
+++ login/models.py
@@ -5,4 +5,4 @@ class User(models.Model):
 	password = models.CharField(max_length=20)
 	cashMoney = models.IntegerField(default=0)
 	def __str__(self):
-		return self.username+" has $"+cashMoney
+		return self.username+" has $"+str(self.cashMoney)
